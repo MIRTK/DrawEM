9151b8429b9ac266efff390855962f2c599c83cc
amakropoulos <benuix@gmail.com>
2020-10-21 17:08:09 +0300

    MCRIB segmentation

--- scripts/segmentation.sh
+++ scripts/segmentation.sh
@@ -28,7 +28,9 @@ if [ ! -f segmentations/$subj-initial.nii.gz ];then
 sdir=segmentations-data
 
 subcorts=`cat $DRAWEMDIR/parameters/subcortical-all.csv`
-tissues="outlier csf gm wm hwm lwm"
+tissues="outlier csf gm wm"
+#TODO
+# tissues="outlier csf gm wm hwm lwm"
 
 
 mkdir -p segmentations $sdir/posteriors logs || exit 1;
@@ -50,17 +52,18 @@ for str in ${tissues};do
 structs="$structs $sdir/labels/$str/$subj.nii.gz";
 post=$sdir/posteriors/$str/$subj.nii.gz
 posts="$posts $post "
-saveposts="$saveposts -saveprob $num $post "; 
+saveposts="$saveposts -saveprob $num $post ";
 num=$(($num+1));
 done
 
 
-
 # segmentation
-run mirtk draw-em N4/$subj.nii.gz 27 $structs segmentations/$subj-em.nii.gz -padding 0 -mrf $DRAWEMDIR/parameters/connectivities.mrf -tissues 1 21 1 22 1 23 3 24 25 26  -hui -postpenalty $sdir/MADs/$subj-subspace.nii.gz $saveposts 1>logs/$subj-em 2>logs/$subj-em-err
+#TODO
+run mirtk draw-em N4/$subj.nii.gz $num $structs segmentations/$subj-em.nii.gz -padding 0 -mrf $DRAWEMDIR/parameters/connectivities.mrf -tissues 1 27 1 28 1 29 1 30  -hui -postpenalty $sdir/MADs/$subj-subspace.nii.gz $saveposts 1>logs/$subj-em 2>logs/$subj-em-err
 
 # add hwm and lwm posterior probability to wm
-run mirtk calculate $sdir/posteriors/hwm/$subj.nii.gz -add $sdir/posteriors/lwm/$subj.nii.gz -add $sdir/posteriors/wm/$subj.nii.gz -out $sdir/posteriors/wm/$subj.nii.gz
+#TODO
+# run mirtk calculate $sdir/posteriors/hwm/$subj.nii.gz -add $sdir/posteriors/lwm/$subj.nii.gz -add $sdir/posteriors/wm/$subj.nii.gz -out $sdir/posteriors/wm/$subj.nii.gz
 
 # posteriors [0,1] -> [0,100]
 for post in ${posts};do
