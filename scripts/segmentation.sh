b852b0515368d077eeacbb9d942c2cbc2094a2fb
amakropoulos <benuix@gmail.com>
2020-10-25 01:50:46 +0300

    rewrite segmentation function-based

--- scripts/segmentation.sh
+++ scripts/segmentation.sh
@@ -23,67 +23,85 @@
 [ $# -eq 1 ] || { echo "usage: $(basename "$0") <subject>"; exit 1; }
 subj=$1
 
-
-if [ ! -f segmentations/$subj-initial.nii.gz ];then
 sdir=segmentations-data
 
-mkdir -p segmentations $sdir/posteriors logs || exit 1;
-for r in ${NONCORTICAL};do mkdir -p $sdir/posteriors/seg$r || exit 1; done
-for str in ${ATLAS_TISSUES};do mkdir -p $sdir/posteriors/$str || exit 1; done
-
-
-structs=""; saveposts=""; posts=""; num=0;
-# subcortical
-for r in ${NONCORTICAL};do 
-structs="$structs $sdir/labels/seg$r/$subj.nii.gz";
-post=$sdir/posteriors/seg$r/$subj.nii.gz
-posts="$posts $post "
-saveposts="$saveposts -saveprob $num $post "; 
-num=$(($num+1)); 
-done
-# tissues
-tissue_num=$num
+structures=""; save_posteriors=""; posteriors=""; num=-1;
 tissues_parameter=""
-for group in ${ATLAS_TISSUES_GROUPING};do
-    tissues_parameter="$tissues_parameter $group"
-    for ((group_i=0; group_i<$group; group_i++));do
-        tissues_parameter="$tissues_parameter $tissue_num"
-        tissue_num=$(($tissue_num+1))
+em_labels_to_all_labels=""
+
+add_structure()
+{
+    structure=$1
+    mkdir -p $sdir/posteriors/$structure || exit 1;
+    num=$(($num+1)); 
+    structures="$structures $sdir/labels/$structure/$subj.nii.gz";
+    posterior=$sdir/posteriors/$structure/$subj.nii.gz
+    posteriors="$posteriors $posterior "
+    save_posteriors="$save_posteriors -saveprob $num $posterior "; 
+}
+
+add_noncortical()
+{
+    structure_nr=$1
+    add_structure seg$structure_nr;
+    em_labels_to_all_labels="$em_labels_to_all_labels 1 $(($num+1)) $structure_nr"
+}
+
+add_tissue(){
+    tissue_var=$1
+    output_all_label=$2
+    tissue_labels=${!tissue_var}
+    num_subtissues=`echo $tissue_labels |wc -w`
+    tissues_parameter="$tissues_parameter $num_subtissues"
+    em_labels_to_all_labels="$em_labels_to_all_labels $num_subtissues"
+    for subtissue in $tissue_labels;do
+        add_structure $subtissue;
+        tissues_parameter="$tissues_parameter $num"
+        em_labels_to_all_labels="$em_labels_to_all_labels $(($num+1))"
     done
-done
+    em_labels_to_all_labels="$em_labels_to_all_labels $output_all_label"
+}
 
-for str in ${ATLAS_TISSUES};do
-structs="$structs $sdir/labels/$str/$subj.nii.gz";
-post=$sdir/posteriors/$str/$subj.nii.gz
-posts="$posts $post "
-saveposts="$saveposts -saveprob $num $post ";
-num=$(($num+1));
-done
-
-
-# segmentation
-run mirtk draw-em N4/$subj.nii.gz $num $structs segmentations/$subj-em.nii.gz -padding 0 -mrf $CONNECTIVITIES -tissues $tissues_parameter -hui -postpenalty $sdir/MADs/$subj-subspace.nii.gz $saveposts 1>logs/$subj-em 2>logs/$subj-em-err
-
-# add posterior probability of sub-tissues to tissues
-atlas_tissues_arr=($ATLAS_TISSUES)
-tissue_num=0
-for group in ${ATLAS_TISSUES_GROUPING};do
+add_tissue_posteriors(){
+    # add posterior probability of sub-tissues to tissues
+    tissue_var=$1
+    output_tissue=$2
+    tissue_labels=${!tissue_var}
     addem=""
-    out=$sdir/posteriors/${atlas_tissues_arr[$tissue_num]}/$subj.nii.gz
-    for ((group_i=0; group_i<$group; group_i++));do
-        addem=$addem"-add $sdir/posteriors/${atlas_tissues_arr[$tissue_num]}/$subj.nii.gz ";
-        tissue_num=$(($tissue_num+1))
+    for subtissue in $tissue_labels;do
+        addem=$addem"-add $sdir/posteriors/$subtissue/$subj.nii.gz ";
     done
-    if [ $group -gt 1 ];then
-        addem=`echo $addem|sed -e 's:^-add::g'`
-        run mirtk calculate $addem -out $out
-    fi
-done
+    addem=`echo $addem|sed -e 's:^-add::g'`
+    run mirtk calculate $addem -out $sdir/posteriors/$output_tissue/$subj.nii.gz
+}
 
-# posteriors [0,1] -> [0,100]
-for post in ${posts};do
-run mirtk calculate $post -mul 100 -out $post 
-done
 
-run mirtk padding segmentations/$subj-em.nii.gz segmentations/$subj-em.nii.gz segmentations/$subj-initial.nii.gz $EM_LABELS_TO_ALL_LABELS
+if [ ! -f segmentations/$subj-initial.nii.gz ];then
+    # subcortical
+    for r in ${NONCORTICAL};do
+        add_noncortical $r 
+    done
+    # tissues
+    add_tissue OUTLIER_TISSUES $OUTLIER_LABEL
+    add_tissue CSF_TISSUES $CSF_LABEL
+    add_tissue GM_TISSUES $SUPER_GM_LABEL
+    add_tissue WM_TISSUES $SUPER_WM_LABEL
+
+    # segmentation
+    mkdir -p segmentations || exit 1;
+    num_structures=$(($num+1))
+    run mirtk draw-em N4/$subj.nii.gz $num_structures $structures segmentations/$subj-em.nii.gz -padding 0 -mrf $CONNECTIVITIES -tissues $tissues_parameter -hui -postpenalty $sdir/MADs/$subj-subspace.nii.gz $save_posteriors 1>logs/$subj-em 2>logs/$subj-em-err
+
+    # posteriors [0,1] -> [0,100]
+    for post in ${posteriors};do
+        run mirtk calculate $post -mul 100 -out $post 
+    done
+
+    # add posterior probability of sub-tissues to tissues
+    add_tissue_posteriors OUTLIER_TISSUES outlier
+    add_tissue_posteriors CSF_TISSUES csf
+    add_tissue_posteriors GM_TISSUES gm
+    add_tissue_posteriors WM_TISSUES wm
+
+    run mirtk padding segmentations/$subj-em.nii.gz segmentations/$subj-em.nii.gz segmentations/$subj-initial.nii.gz $em_labels_to_all_labels
 fi
